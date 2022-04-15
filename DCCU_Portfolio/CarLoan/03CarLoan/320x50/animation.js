f67d564f8dcf3ca51dee76d4bfadd8a22f6d193b
shanedsgn <shanedsgn@gmail.com>
2022-04-15 11:57:20 -0400

    Add files via upload

---
+++ DCCU_Portfolio/CarLoan/03CarLoan/320x50/animation.js
@@ -0,0 +1,20 @@
+
+var tl = gsap.timeline({delay:0.2});
+
+gsap.ticker.fps(24);
+
+tl
+.to("#hero",{duration: 0.4, scale:0.5, opacity:1, ease:"power2.easeOut"},"-=0.2")
+.to(".txt.one", {duration: 0.8, left:0, ease: "elastic.out(0.9, 0.7)", opacity:1, stagger: 0.1 },"-=0.1")
+.to("#txt2", {duration: 0.8, left:0, ease: "elastic.out(0.9, 0.6)", opacity:1, stagger: 0.08 },"+=0.6")
+.to("#hero",{duration: 0.8, left: -17, ease:"power3.out"},"+=1")
+.to(".txt",{duration: 0.5, left: -24, top:-4, ease:"power3.out"},"-=0.8")
+.to(".bubbles-ani", {duration: 1, scale:1, ease: "elastic.out(1, 0.3)", opacity:1, stagger: 0.15 },"-=1")
+.to("#cta",{duration: 0.7, top:0, ease: "elastic.out(0.9, 0.9)", opacity: 1},"=-1")
+.to("#disclaimer",{duration: 0.4, ease:"power2.out", opacity: 1},"-=0.7")
+;
+
+// tl.seek("seek", true);
+// tl.seek(5);
+
+console.log(tl.duration());
