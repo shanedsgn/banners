1a153c5eabbcfdea8102e28f7dc361a106f33803
shanedsgn <shanedsgn@gmail.com>
2023-04-24 20:49:49 -0400

    Add files via upload

---
+++ GL_Portfolio/EG/EG_Set1_160x600/scripts/animation.js
@@ -0,0 +1,28 @@
+
+var tl = gsap.timeline({delay:0.3, repeat: 2});
+var repeat = tl.repeatDelay(); 
+
+gsap.ticker.fps(24);
+
+tl
+.to("#gl_logo", {duration: 0.7, scale:0.5, opacity:1, force3D:false, ease: "elastic.out(0.9, 0.7)"},"-=0")
+
+.to("#child",{duration: 0.6, top: 167, opacity:1, ease:"power4.out"},"-=0.5")
+.to("#card_shell", {duration: 0.7, scale:0.5, opacity:1, stagger: 0.1, force3D:false, ease: "elastic.out(0.9, 0.7)"},"-=0.3")
+.to("#card_overlay",{duration: 0.1, opacity:1, ease:"sine.inOut"},"-=0.6")
+
+.to(".copy-build-1", {duration: 0.7, left: 7, opacity:1, stagger: 0.1, ease:"elastic.out(0.4, 0.7)"},"-=0.5")
+
+.to("#milestone1", {duration: 0.7, scale:0.5, opacity:1, stagger: 0.1, force3D:false, ease: "elastic.out(0.9, 0.7)"},"-=0.5")
+.to("#milestone2", {duration: 0.4, top:238, left: 53, opacity:1, force3D:false, rotation: "19", ease: "back.out(1)"},"-=0.5")
+
+.to("#cta",{duration: 0.7, left: 5, opacity:1, ease:"elastic.out(0.4, 0.7)"},"-=0")
+;
+
+tl.repeatDelay(2); 
+
+//checks entire duration of the animation (view via inspect element in browser)
+console.log("duration is: " + tl.duration());
+
+// tl.seek("seek", true);
+// tl.seek(1);
