5c1ff4dabde4a81d73afc987de03c3cf3b5ae619
shanedsgn <shanedsgn@gmail.com>
2024-12-24 20:45:12 -0500

    Add files via upload

---
+++ SSB_Portfolio/BusinessBrokers/BB_300x600/animation.js
@@ -0,0 +1,42 @@
+
+var tl = gsap.timeline({delay:0.2});
+
+gsap.ticker.fps(24);
+
+tl
+//Scene 01
+.to("#txt1_a", {duration: 0.5, top:-5, ease: "back.out(0.8)", opacity:1},"+=0")
+.to("#txt1_b", {duration: 0.5, top:2, ease: "back.out(0.8)", opacity:1},"-=0.5")
+
+.to(".puzzle-01", {duration: 0.5, scale:0.5, ease: "power3.out", opacity:1, stagger: 0.1, repeat:1, repeatDelay:0.8, yoyo:true},"+=0.7")
+.to(".puzzle-02", {duration: 0.5, scale:0.5, ease: "power3.out", opacity:1, stagger: 0.1, repeat:1, repeatDelay:0.8, yoyo:true},"-=2")
+.to(".puzzle-03", {duration: 0.5, scale:0.5, ease: "power3.out", opacity:1, stagger: 0.1, repeat:1, repeatDelay:0.8, yoyo:true},"-=2")
+.to("#text01_wrapper",{duration: 0.4, ease:"power3.in", opacity:0},"-=2.2")
+
+//Scene 02
+.to("#txt2_a", {duration: 0.5, top:1, ease: "back.out(0.8)", opacity:1},"-=2")
+.to("#txt2_b", {duration: 0.5, top:-14, ease: "back.out(0.8)", opacity:1},"-=2")
+.to("#blue",{duration: 0.4, ease:"power3.out", opacity:1},"-=0.3")
+.to("#text02_wrapper",{duration: 0.5, left:-300, ease:"power3.in", opacity:1},"-=0.5")
+
+//Scene 03
+.to("#photo_wrapper",{duration: 0.4, ease:"power3.out", opacity:1},"-=0.2")
+.to("#photo",{duration: 7, scale:0.55, ease:"Sine.easeOut", opacity:1, force3D:false},"-=0.3")
+
+.to(".puzzle-01", {duration: 0.5, scale:0.5, ease: "power3.out", opacity:1, stagger: 0.1, repeat:1, repeatDelay:0.8, yoyo:true},"-=4.5")
+.to(".puzzle-02", {duration: 0.5, scale:0.5, ease: "power3.out", opacity:1, stagger: 0.1, repeat:1, repeatDelay:0.8, yoyo:true},"-=4.3")
+.to(".puzzle-03", {duration: 0.5, scale:0.5, ease: "power3.out", opacity:1, stagger: 0.1, repeat:1, repeatDelay:0.8, yoyo:true},"-=4.3")
+
+.to("#photo_wrapper",{duration: 0.4, ease:"power3.out", opacity:0},"-=4.1")
+
+//Scene 04
+.to("#txt3_a", {duration: 0.5, scale: 0.5, ease: "back.out(0.8)", opacity:1},"-=3.6")
+.to("#txt3_b", {duration: 0.5, scale:0.5, ease: "back.out(0.8)", opacity:1},"-=3.1")
+.to("#logo_symbol", {duration: 0.4, scale:0.5, ease: "back.out(0.8)", opacity:1, force3D:false},"-=3.9")
+.to("#cta", {duration: 0.5, scale:0.5, ease: "back.out(0.8)", opacity:1, force3D:false},"-=1.6")
+.to("#logo_main",{duration: 0.5, ease:"power3.out", opacity:1 },"-=1.4");
+
+// // tl.seek("seek", true);
+// tl.seek(6);
+
+console.log(tl.duration());
