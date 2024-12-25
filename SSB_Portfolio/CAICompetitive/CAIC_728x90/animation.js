3fdb1cc1016a93f1605fbe46de169e2c26f7a260
shanedsgn <shanedsgn@gmail.com>
2024-12-24 19:07:49 -0500

    Add files via upload

---
+++ SSB_Portfolio/CAICompetitive/CAIC_728x90/animation.js
@@ -0,0 +1,30 @@
+
+var tl = gsap.timeline({delay:0.2});
+
+gsap.ticker.fps(24);
+
+tl
+//Scene 01
+// .to("#bank", {duration: 0.7, left:5, ease: "back.out(0.3)", opacity:1, yoyo:true, repeat:1, repeatDelay:2.4, force3D:false},"-=0")
+.to("#txt1_a", {duration: 0.55, scale:0.5, ease: "back.out(1)", opacity:1},"-=0")
+.to("#txt1_b", {duration: 0.55, top:32, ease: "back.out(1)", opacity:1},"-=0")
+.to("#txt1_c", {duration: 0.55, left:3, ease: "back.out(1)", opacity:1},"-=0")
+.to("#text01_wrapper",{duration: 0.5, left: 300, ease:"sine.in", opacity:0, force3D:false},"+=1")
+
+//Scene 02
+.to("#txt2_a", {duration: 0.55, scale:0.5, ease: "back.out(1)", opacity:1},"-=0.3")
+.to("#txt2_b", {duration: 0.55, top:32, ease: "back.out(1)", opacity:1},"-=0")
+.to("#txt2_c", {duration: 0.55, left:7, ease: "back.out(1)", opacity:1},"-=0")
+.to("#txt2_d", {duration: 0.55, scale:0.5, ease: "back.out(1)", opacity:1},"-=0")
+.to("#text02_wrapper",{duration: 0.5, left: 300, ease:"sine.in", opacity:0, force3D:false},"+=1")
+.to("#bank, #blue", {duration: 0.55, ease: "back.out(1)", opacity:0},"-=0.3")
+
+//Scene 03
+.to("#txt3", {duration: 0.6, scale:0.5, ease: "back.out(0.8)", opacity:1},"-=0.4")
+.to("#cta", {duration: 0.5, scale:0.5, ease: "back.out(0.8)", opacity:1, force3D:false},"+=0")
+.to("#logo_main",{duration: 0.5, ease:"power3.out", opacity:1 },"-=0");
+
+// tl.seek("seek", true);
+// tl.seek(8);
+
+console.log(tl.duration());
